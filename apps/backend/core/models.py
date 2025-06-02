3609b3f65bb95143c73923884ae7271504523d9a
Steven <steven.ola@icm-institute.org>
2025-06-02 16:02:34 +0200

    fixes: refactor: clean up model definitions and improve readability

--- apps/backend/core/models.py
+++ apps/backend/core/models.py
@@ -3,55 +3,73 @@ from django.conf import settings
 
 
 class User(models.Model):
-    id: int = models.AutoField(primary_key=True, verbose_name="User ID")
-    name: str = models.CharField(verbose_name="Name of the user", max_length=100)
-    is_organizer: bool = models.BooleanField(default=False, verbose_name="Is organizer")
+    name = models.CharField(verbose_name="Name of the user", max_length=100)
+    is_organizer = models.BooleanField(default=False, verbose_name="Is organizer")
+
+    def __str__(self):
+        return self.name
+
+
+class Proposal(models.Model):
+    description = models.TextField(verbose_name="Proposal Description")
+    creator = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='proposals'
+    )
+
+    def __str__(self):
+        return f"Proposal {self.id} by {self.creator.name}"
+
 
 class Vote(models.Model):
     PRO = "PR"
     CON = "CO"
-    TYPE_CHOICES = {
-       PRO: "pro",
-       CON: "con"
-    }
-    type: str = models.CharField(
+    TYPE_CHOICES = [
+        (PRO, "pro"),
+        (CON, "con"),
+    ]
+
+    type = models.CharField(
         max_length=2,
         choices=TYPE_CHOICES,
-        default=PRO
+        default=PRO,
+        verbose_name="Vote Type"
     )
-    comment: str = models.TextField(verbose_name="Comment", blank=True, null=True)
-    user: User = models.ForeignKey(User, on_delete=models.CASCADE, related_name="votes")
-
-
-class Proposal(models.Model):
-    description: str
-    creator: User
-    votes: Vote = models.ManyToManyField(
-        Vote,
-        related_name="proposals",
-        blank=True
-    )
-    creator: User = models.ForeignKey(
-        User,
+    comment = models.TextField(verbose_name="Comment", blank=True)
+    user = models.ForeignKey(
+        'User',
         on_delete=models.CASCADE,
-        related_name="proposals"
+        related_name="votes"
     )
+    proposal = models.ForeignKey(
+        'Proposal',
+        on_delete=models.CASCADE,
+        related_name="votes"
+    )
+
+    def __str__(self):
+        return f"{self.get_type_display()} by {self.user.name}"
+
 
 class Decision(models.Model):
-    duration: int = models.IntegerField(verbose_name="Duration in seconds")
-    number_of_participants: int = models.IntegerField(verbose_name="Number of participants")
-    title: str = models.CharField(verbose_name="Title of the decision")
-    context: str = models.TextField(verbose_name="Additionnal context")
-    organizer: User = models.ForeignKey(
-        User,
+    duration = models.IntegerField(verbose_name="Duration in seconds")
+    number_of_participants = models.IntegerField(verbose_name="Number of participants")
+    title = models.CharField(max_length=255, verbose_name="Title of the decision")
+    context = models.TextField(verbose_name="Additional context")
+    organizer = models.ForeignKey(
+        'User',
         on_delete=models.CASCADE,
         related_name="decisions"
     )
-    proposals: Proposal = models.ManyToManyField(
-        Proposal,
+    proposals = models.ManyToManyField(
+        'Proposal',
         related_name="decisions",
         blank=True
     )
+
+    def __str__(self):
+        return self.title
 
     @property
     def link(self):
