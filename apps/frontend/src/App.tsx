34565233d89fb24a4bf028aa2710f83602d6240e
Alexandre Tang <a2@jupi.co>
2025-06-03 10:35:45 +0200

    chore: typo

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,11 +2,13 @@ import './App.css'
 import { InitScreen } from './screens/InitScreen'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import { StepsHandler } from './components/StepsHandler'
+import { WelcomeScreen } from './screens/WelcomeScreen'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<WelcomeScreen/> } />
         <Route path="/init" element={<InitScreen />} />
         <Route path="/decision/:uuid" element={<StepsHandler />} />
       </Routes>
